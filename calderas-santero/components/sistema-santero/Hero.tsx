ab2be7ac94e0d840fee3e2264d1ab19fcd1730eb
chiarasalvu <chiarasalvucci1@gmail.com>
2026-07-14 19:16:18 -0300

    cambios sistema santero

--- calderas-santero/components/sistema-santero/Hero.tsx
+++ calderas-santero/components/sistema-santero/Hero.tsx
@@ -7,9 +7,7 @@ export default function Hero() {
         <div>
           <h1 className="font-heading text-3xl font-bold text-white sm:text-5xl">
             Sistema Santero:
-            <span className="block text-brand-red">
-              Eficiencia Térmica Reinventada
-            </span>
+            <span className="block">Eficiencia Térmica Reinventada</span>
           </h1>
 
           <p className="mt-6 max-w-md text-white/80">
